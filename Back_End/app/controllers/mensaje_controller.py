a65636c977d0b77ea9ff71c0a285003fa0884c63
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-09-25 17:09:17 -0300

    Mensaje controller
    Terminamos de escribir lo basico de mensaje controller

---
+++ Back_End/app/controllers/mensaje_controller.py
@@ -0,0 +1,81 @@
+from ..models.mensaje_model import Mensaje
+
+from flask import request
+
+
+from ..routes.error_handlers import handle_film_not_found
+
+
+
+class MensajeController:
+    """Film controller class"""
+
+    @classmethod
+    def get(cls, id_mensaje):
+        """Get a mensaje by id"""
+        mensaje = Mensaje(id_mensaje=id_mensaje)
+        result = Mensaje.get(mensaje)
+
+        if result is not None:
+            return result.serialize(), 200
+
+
+
+        
+    @classmethod
+    def get_all(cls):
+        """Get all mensajes"""
+        mensaje_objects = Mensaje.get_all()
+        mensajes = []
+        for mensaje in mensaje_objects:
+            mensajes.append(mensaje.serialize())
+        return mensajes, 200
+    
+    
+    @classmethod
+    def create(cls):
+        """Create a new mensaje"""
+        data = request.json
+        # TODO: Validate data
+        # if data.get('rental_rate') is not None:
+        #     if isinstance(data.get('rental_rate'), int):
+        #         data['rental_rate'] = Decimal(data.get('rental_rate'))/100
+        
+        # if data.get('replacement_cost') is not None:
+        #     if isinstance(data.get('replacement_cost'), int):
+        #         data['replacement_cost'] = Decimal(data.get('replacement_cost'))/100
+
+        mensaje = Mensaje(**data)
+        Mensaje.create(mensaje)
+        return {'message': 'Mensaje created successfully'}, 201
+
+
+    @classmethod
+    def update(cls, id_mensaje):
+        """Update a mensaje"""
+        data = request.json
+        # TODO: Validate data
+        # if data.get('rental_rate') is not None:
+        #     if isinstance(data.get('rental_rate'), int):
+        #         data['rental_rate'] = Decimal(data.get('rental_rate'))/100
+        
+        # if data.get('replacement_cost') is not None:
+        #     if isinstance(data.get('replacement_cost'), int):
+        #         data['replacement_cost'] = Decimal(data.get('replacement_cost'))/100
+        
+        data['id_mensaje'] = id_mensaje
+
+        mensaje = Mensaje(**data)
+
+        # TODO: Validate film exists
+        Mensaje.update(mensaje)
+        return {'message': 'Mensaje updated successfully'}, 200
+    
+    @classmethod
+    def delete(cls, id_mensaje):
+        """Delete a mensaje"""
+        mensaje = Mensaje(id_mensaje=id_mensaje)
+
+        # TODO: Validate film exists
+        Mensaje.delete(mensaje)
+        return {'message': 'Mensaje deleted successfully'}, 204
